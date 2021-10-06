35c9efd27f3a8a626cc32871eafa9b394ea44a8a
TadamiNagahama <80555118+TadamiNagahama@users.noreply.github.com>
2021-10-07 06:34:50 +0900

    Add files via upload

---
+++ clinic/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+{
+  $(".news4").click(function(){
+    $(".news4-p").toggleClass('active');
+    return false;
+  });
+  $(".news3").click(function(){
+    $(".news3-p").toggleClass('active');
+    return false;
+  });
+  $(".news2").click(function(){
+    $(".news2-p").toggleClass('active');
+    return false;
+  });
+  $(".news1").click(function(){
+    $(".news1-p").toggleClass('active');
+    return false;
+  });
+}
