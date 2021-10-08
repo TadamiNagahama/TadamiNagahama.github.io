8910f95e4aa0ff32093874d4e9b8ca7f5e83ceba
TadamiNagahama <80555118+TadamiNagahama@users.noreply.github.com>
2021-10-09 07:17:14 +0900

    Add files via upload

--- clinic/js/main.js
+++ clinic/js/main.js
@@ -2,19 +2,23 @@
 
 {
   $(".news4").click(function(){
-    $(".news4-p").toggleClass('active');
+    // $(".news4-p").toggleClass('active');
+    $(".news4-p").slideToggle(300);
     return false;
   });
   $(".news3").click(function(){
-    $(".news3-p").toggleClass('active');
+    // $(".news3-p").toggleClass('active');
+    $(".news3-p").slideToggle(300);
     return false;
   });
   $(".news2").click(function(){
-    $(".news2-p").toggleClass('active');
+    // $(".news2-p").toggleClass('active');
+    $(".news2-p").slideToggle(300);
     return false;
   });
   $(".news1").click(function(){
-    $(".news1-p").toggleClass('active');
+    // $(".news1-p").toggleClass('active');
+    $(".news1-p").slideToggle(300);
     return false;
   });
 }
